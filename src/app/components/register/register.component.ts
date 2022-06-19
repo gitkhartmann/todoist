1ac9f93ac3b1ff0e6ebecf5621adc9c5e225eae6
gitkhartmann <gitkhartmann@gmail.com>
2022-06-19 23:45:03 +0300

    started working on the registration page

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MyErrorStateMatcher } from '../log-in/log-in.component';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  logInForm!: FormGroup
 
-  constructor() { }
+  matcher = new MyErrorStateMatcher();
+  
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.logInForm = new FormGroup({
+			emailFormControl: new FormControl('', [Validators.required, Validators.email]),
+      passwordFormControl: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+		})
   }
+
+  submit() {
+    console.dir(this.logInForm.value.emailFormControl,this.logInForm.value.passwordFormControl )
+  }
+
+  reset() {
+    this.logInForm.reset()
+  }
+
+  goToLoginPage() {
+    this.router.navigate(['login'])
+  }
+
 
 }
