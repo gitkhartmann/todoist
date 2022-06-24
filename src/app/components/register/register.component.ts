f01d472c71ef160d4ac4ffdc7ba85acf2aa6bd9a
gitkhartmann <gitkhartmann@gmail.com>
2022-06-24 13:40:39 +0300

    feat: добавлена отписка от подписок в компоненте

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,8 +1,9 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { MyErrorStateMatcher } from '../log-in/log-in.component';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-register',
@@ -10,52 +11,53 @@ import { MyErrorStateMatcher } from '../log-in/log-in.component';
   styleUrls: ['./register.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, OnDestroy {
+  destroy$: Subject<boolean> = new Subject<boolean>();
+  registerForm!: FormGroup;
+  loading: boolean = false;
+  matcher = new MyErrorStateMatcher();
 
-  registerForm!: FormGroup
-  loading: boolean = false
-  matcher = new MyErrorStateMatcher()
-
-  constructor(
-    private router: Router,
-    private auth: AuthService
-  ) { }
+  constructor( private auth: AuthService ) { }
+  
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
 
   ngOnInit(): void {
     this.registerForm = new FormGroup({
-      nameFormControl: new FormControl('',[Validators.required, Validators.minLength(2)]),
-			emailFormControl: new FormControl('', [Validators.required, Validators.email]),
+      nameFormControl: new FormControl('', [Validators.required, Validators.minLength(2)]),
+      emailFormControl: new FormControl('', [Validators.required, Validators.email]),
       passwordFormControl: new FormControl('', [Validators.required, Validators.minLength(6)]),
-    })
-    this.auth.canAuthenticate()
+    });
+    this.auth.canAuthenticate();
   }
 
-  submit() {
-    this.loading = true
+  submit(): void {
+    this.loading = true;
     this.auth.registerUser(
       this.registerForm.value.nameFormControl,
       this.registerForm.value.emailFormControl,
       this.registerForm.value.passwordFormControl
-    ).subscribe({
+    ).pipe(takeUntil(this.destroy$))
+      .subscribe({
       next: data => {
-        this.reset()
-        //сохраняем токен в localStorage
-        /*this.auth.tokenInStorage(data.idToken)*/
-        this.auth.setToken(data)
-        console.log('Registered IdToken', data.idToken, data.expiresIn)
-        this.auth.canAuthenticate()
+          this.reset();
+          this.auth.setToken(data);
+          console.log('Registered IdToken', data.idToken, data.expiresIn);
+          this.auth.canAuthenticate();
       },
       error: data => {
-        console.log(data.error.error.message)
+        console.log(data.error.error.message);
       }
     }).add(() => {
-      this.loading = false
-      console.log('Registered complete')
+      this.loading = false;
+      console.log('Registered complete');
     })
   }
 
-  reset() {
-    this.registerForm.reset()
+  reset(): void {
+    this.registerForm.reset();
   }
 
   
