a24ea1e70b6db4ca53f4f73fb3a165fa5ec67ee0
gitkhartmann <gitkhartmann@gmail.com>
2022-06-27 22:47:21 +0300

    test: добавлен файл с тестами

---
+++ src/app/shared/back-end.interceptor.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { BackEndInterceptor } from './back-end.interceptor';
+
+describe('BackEndInterceptor', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      BackEndInterceptor
+      ]
+  }));
+
+  it('should be created', () => {
+    const interceptor: BackEndInterceptor = TestBed.inject(BackEndInterceptor);
+    expect(interceptor).toBeTruthy();
+  });
+});
