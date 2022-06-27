1d6471c34cce2a009f977a3367b27028ad271e0e
gitkhartmann <gitkhartmann@gmail.com>
2022-06-27 20:03:37 +0300

    feat: добавлен путь в роутинге

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LogInComponent },
   { path: 'tasktable', component: TasksTableComponent },
+  { path: '**', component: TasksTableComponent },
 ];
 
 @NgModule({
