955492ac8aaa60dbef3682e032d96de6fe5a3be5
gitkhartmann <gitkhartmann@gmail.com>
2022-06-28 12:30:30 +0300

    refactor: удалён лишний код

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -7,7 +7,6 @@ import { IFbCreateResponse, ITask } from '../shared/interfaces';
   providedIn: 'root'
 })
 export class TaskService {
-
   public flag$ = new Subject<boolean>();
 
   constructor(private http: HttpClient) { }
@@ -15,19 +14,19 @@ export class TaskService {
   public changeFlag(flag: boolean) {
     this.flag$.next(flag); 
   }
+  
   create(task: ITask): Observable<ITask> {
     return this.http.post('/tasktable', task)
       .pipe(map((_response: IFbCreateResponse) => {
-          return {
-            ...task
-          }
-        })
-      )
+        return {
+          ...task
+        }
+      })
+      );
   }
 
   update(task: ITask): Observable<ITask> {
-    console.log('ПРОВЕРКА ЭДИТ ТАСК В UPDATE', task.id)
-    return this.http.patch<ITask>('/tasktable', task)
+    return this.http.patch<ITask>('/tasktable', task);
   }
 
   getAll(): Observable<ITask[]> {
@@ -38,52 +37,11 @@ export class TaskService {
           .map(key => ({
             ...response[key]
           }))
-        })
-      )
+      })
+      );
   }
 
   remove(id: string | number): Observable<Object> {
     return this.http.delete('/tasktable', {body: id});
   }
-
-  /*
-  create(task: ITask): Observable<ITask> {
-    return this.http.post(`${environment.fbDbUrl}tasks.json`, task)
-      .pipe(map((response: IFbCreateResponse) => {
-          return {
-            ...task,
-            id: response.name
-          }
-        })
-      )
-  }
-
-  getAll(): Observable<ITask[]> {
-    return this.http.get(`${environment.fbDbUrl}tasks.json`)
-      .pipe(map((response: { [key: string]: any }) => {
-        return Object.keys(response)
-          .map(key => ({
-            ...response[key],
-            id: key
-          }))
-        })
-      )
-  }
-  remove(id: string): Observable<void> {
-    return this.http.delete<void>(`${environment.fbDbUrl}tasks/${id}.json`)
-    
-  }
-
-  getById(id: string): Observable<ITask> {
-		return this.http.get<ITask>(`${environment.fbDbUrl}tasks/${id}.json`)
-			.pipe(map((task: ITask) => {
-				return {
-					...task, id,
-				}
-			}))
-  }
-  
-  update(task: ITask): Observable<ITask> {
-    return this.http.patch<ITask>(`${environment.fbDbUrl}tasks/${task.id}.json`, task)
-  }*/
 }
