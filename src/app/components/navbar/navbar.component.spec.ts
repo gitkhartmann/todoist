654643f6d30c9dd897d8daa9b9838685651d8717
gitkhartmann <gitkhartmann@gmail.com>
2022-06-29 10:00:19 +0300

    test: добавлены тесты компоненту navbar

---
+++ src/app/components/navbar/navbar.component.spec.ts
@@ -0,0 +1,31 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule } from '@angular/material/dialog';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NavbarComponent } from './navbar.component';
+
+
+
+
+
+
+describe('NavbarComponent', () => {
+  let component: NavbarComponent;
+  let fixture: ComponentFixture<NavbarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports:[ MatDialogModule, RouterTestingModule, HttpClientTestingModule],
+      declarations: [ NavbarComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(NavbarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
